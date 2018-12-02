d409082172cd25619c3849855d3c6f1a6fa366e7
Eddy Jo <edwardjo811@gmail.com>
2018-12-02 14:07:27 -0500

    add class binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,14 +9,41 @@ import { Component, OnInit } from '@angular/core';
     <input [id]="myId" type="text" value="Eddy">
     <input bind-disabled="isDisabled" id="{{ myId }}" type="text" value="Eddy">
 
+    <h2 class="text-success">Codevolution</h2>
+    <h2 [class]="successClass">Codevolution</h2>
+    
+    <h2 class="text-special" [class]="successClass">Codevolution</h2>
+    <h2 [class.text-danger]="hasError">Codevolution</h2>
+
+    <h2 [ngClass]="messageClasses">Codevolution</h2>
+
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color: green;
+    }
+    .text-danger {
+      color: red;
+    }
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Eddy";
   public myId = "testId";
   public isDisabled = false;
+
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
   constructor() { }
 
   ngOnInit() {
